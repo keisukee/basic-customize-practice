26e39a5e89f81cb0e92b4fe372716d80d60b4120
keisuke ohashi <kog71903@gmail.com>
2020-04-29 22:43:13 +0900

    Create about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from '../components/header'
+
+export default () => (
+  <div>
+    <Header headerText='siteTitle' />
+    <div style={{ color: `teal` }}>
+      <h1>About Gatsby</h1>
+      <p>Such wow. Very React.</p>
+    </div>
+  </div>
+)
