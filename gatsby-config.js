a9ca4f5a5c774b85f9ae4c7c0de806c9490bc19c
keisuke ohashi <kog71903@gmail.com>
2020-04-30 22:39:39 +0900

    Introduce graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Pands Eating Lots`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -30,5 +30,12 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
   ],
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,23 @@
 import React from "react"
-import Header from '../components/header'
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
 
-export default () => (
-  <div>
-    <Header headerText='siteTitle' />
-    <div style={{ color: `teal` }}>
-      <h1>About Gatsby</h1>
-      <p>Such wow. Very React.</p>
-    </div>
-  </div>
+export default ({ data }) => (
+  <Layout>
+    <h1>About Pandas Eating Lots</h1>
+    <p>
+      We're the only site running on your computer dedicated to showing the best
+      photos and videos of pandas eating lots of food.
+    </p>
+  </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
